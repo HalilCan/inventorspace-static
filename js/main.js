c2fd3599efa230263ddad2e7b5aceebf9820af54
HalilCan <halilcanmemoglu@gmail.com>
2017-10-20 12:48:43 -0400

    Switching backgrounds using js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(function() {
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
 });
+
+let bgSet01 = ["img/slide-1-bg-1.jpeg", "img/slide-1-bg-2.jpeg", "img/slide-1-bg-3.png"]
+let bg01_i = 0;
+setInterval(function() {
+    if (bg01_i >= bgSet01.length) {
+        bg01_i = 0;
+    }
+    document.getElementById("bg01-img").src = bgSet01[bg01_i];
+    bg01_i++;
+}, 3000);
