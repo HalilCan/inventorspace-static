826a8710009c3c25ecde24b617212c6aec9c2b86
HalilCan <halilcanmemoglu@gmail.com>
2017-10-27 11:12:03 -0400

    Centered love-hate well, finally.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 $(function() {
     $('a[href*=#]').on('click', function(e) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            e.preventDefault();
+        
+            // Store hash
+            let hash = this.hash;
+        
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+            
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+        
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+       // $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1000, 'linear');
     });
 });
 
